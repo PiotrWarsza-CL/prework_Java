f96792412918ea27fd018140bb64de2ce48209c0
PiotrWarsza-CL <piotr.warsza@coderslab.pl>
2017-07-25 15:44:53 +0200

    ex 2a

--- a_Zadania/a_Pierwszy_Program/Main2_a.java
+++ a_Zadania/a_Pierwszy_Program/Main2_a.java
@@ -2,8 +2,10 @@
 public class Main2_a {
 
 	public static void main(String[] args) {
-		System.out.println("Pierwszy program JAVA")
+		System.out.println("Pierwszy program JAVA");
 
 	}
 
 }
+
+//Brakowało średnika na końcu w wierszu 5.
